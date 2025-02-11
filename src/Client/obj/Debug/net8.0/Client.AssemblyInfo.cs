21b00bce99cad880915609c345aea4a7aa63dfd0
mathieu <mathieu.pinaud@laplateofrme.io>
2025-02-12 00:15:42 +0100

    test 2

--- src/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ src/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe8c9e5a8e85ae29d6a4f3b8b45665241ccff29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b111922669ee5adfce1acc7823ce03cdd51b18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
