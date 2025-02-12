cf8dfe97644c0087815b916ef500bd4e970130e4
mathieu <mathieu.pinaud@laplateofrme.io>
2025-02-12 16:31:46 +0100

    pageseses

--- src/Mobile/MainPage.xaml.cs
+++ src/Mobile/MainPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿namespace Mobile;
+using Client.Pages;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
+		RootComponents.Add("#app", typeof(Home));
 	}
 }
