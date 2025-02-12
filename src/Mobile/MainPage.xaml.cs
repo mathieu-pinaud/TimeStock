cdcfa94cbe51a551591e3ce685e49b48363bd4eb
mathieu <mathieu.pinaud@laplateofrme.io>
2025-02-12 17:09:31 +0100

    uuuustrre

--- src/Mobile/MainPage.xaml.cs
+++ src/Mobile/MainPage.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace Mobile;
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
 using Client.Pages;
+
+namespace Mobile;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-		RootComponents.Add("#app", typeof(Home));
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+
+        var blazorWebView = this.FindByName<BlazorWebView>("BlazorView");
+        blazorWebView?.RootComponents.Add("#app", typeof(Home));
+    }
 }
